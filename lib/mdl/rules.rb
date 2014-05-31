a58674f03fc846023e9efc934f2213ba6daa274e
Mark Harrison <mark@mivok.net>
2014-05-31 14:46:07 -0400

    Add MD003 rule for mixed header styles

--- lib/mdl/rules.rb
+++ lib/mdl/rules.rb
@@ -19,3 +19,29 @@ rule "MD002", "First header should be a top level header" do
     [first_header[:location]] unless first_header[:level] == 1
   end
 end
+
+rule "MD003", "Mixed header styles" do
+  # Header styles are things like ### and adding underscores
+  # See http://daringfireball.net/projects/markdown/syntax#header
+  check do |doc, lines|
+    doc_style = nil
+    errors = []
+    doc.root.children.select { |e| e.type == :header }.each do |h|
+      line = lines[h.options[:location] - 1]
+      if line.start_with?("#")
+        if line.strip.end_with?("#")
+          style = :atx_closed
+        else
+          style = :atx
+        end
+      else
+        style = :setext
+      end
+      doc_style = style if doc_style.nil?
+      if style != doc_style
+        errors << h.options[:location]
+      end
+    end
+    errors
+  end
+end
