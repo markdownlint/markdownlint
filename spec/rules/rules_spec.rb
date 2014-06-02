7d4904d79c8ef6d0d171c3e5992206e8f52ab77f
Mark Harrison <mark@mivok.net>
2014-06-01 23:06:05 -0400

    Add tests for MD003 and for blank files

--- spec/rules/rules_spec.rb
+++ spec/rules/rules_spec.rb
@@ -4,6 +4,10 @@ describe "rules" do
   let(:rules) { MarkdownLint::RuleSet.load_default }
 
   describe "MD001" do
+    it "doesn't fire on an empty doc" do
+      doc = doc_for ""
+      expect(rules["MD002"].check.call(doc)).to be_nil
+    end
     it "doesn't fire on normal headers" do
       doc = doc_for <<-EOF.unindent
                     # Header
@@ -32,6 +36,10 @@ describe "rules" do
   end
 
   describe "MD002" do
+    it "doesn't fire on an empty doc" do
+      doc = doc_for ""
+      expect(rules["MD002"].check.call(doc)).to be_nil
+    end
     it "doesn't fire on top level ast-style first header" do
       doc = doc_for <<-EOF.unindent
                     # Header
@@ -57,6 +65,93 @@ describe "rules" do
                     ------
                     EOF
       expect(rules["MD002"].check.call(doc)).to eq [1]
+    end
+  end
+
+
+  describe "MD003" do
+    it "doesn't fire on an empty doc" do
+      doc = doc_for ""
+      expect(rules["MD003"].check.call(doc)).to be_nil
+    end
+
+    it "doesn't fire on non-mixed header types (atx)" do
+      doc = doc_for <<-EOF.unindent
+                    # Header
+
+                    ## Header2
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to be_empty
+    end
+    it "doesn't fire on non-mixed header types (atx closed)" do
+      doc = doc_for <<-EOF.unindent
+                    # Header #
+
+                    ## Header2 ##
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to be_empty
+    end
+    it "doesn't fire on non-mixed header types (setext)" do
+      doc = doc_for <<-EOF.unindent
+                    Header
+                    ======
+
+                    Header2
+                    -------
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to be_empty
+    end
+    it "fires on mixed header styles (atx + atx closed)" do
+      doc = doc_for <<-EOF.unindent
+                    # Header
+
+                    ## Header2 ##
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to eq [3]
+    end
+    it "fires on mixed header styles (atx + setext)" do
+      doc = doc_for <<-EOF.unindent
+                    # Header
+
+                    Header2
+                    -------
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to eq [3]
+    end
+    it "fires on mixed header styles (atx closed + setext)" do
+      doc = doc_for <<-EOF.unindent
+                    # Header #
+
+                    Header2
+                    -------
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to eq [3]
+    end
+    it "fires on mixed header styles (atx closed + atx)" do
+      doc = doc_for <<-EOF.unindent
+                    # Header #
+
+                    ## Header2
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to eq [3]
+    end
+    it "fires on mixed header styles (setext + atx)" do
+      doc = doc_for <<-EOF.unindent
+                    Header
+                    ======
+
+                    ## Header2
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to eq [4]
+    end
+    it "fires on mixed header styles (setext + atx closed)" do
+      doc = doc_for <<-EOF.unindent
+                    Header
+                    ======
+
+                    ## Header2 ##
+                    EOF
+      expect(rules["MD003"].check.call(doc)).to eq [4]
     end
   end
 end
