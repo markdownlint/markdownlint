f4b5baf22d2d6338b9ddb8682d0b22bc8d6dfbe2
Mark Harrison <mark@mivok.net>
2014-06-28 10:52:39 -0400

    Document the Markdownlint::Doc class
    This class provides several helper methods that are heavily used in rule
    writing. Document appropriately.

--- lib/mdl/doc.rb
+++ lib/mdl/doc.rb
@@ -1,9 +1,30 @@
 require 'kramdown'
 
 module MarkdownLint
+  ##
   # Representation of the markdown document passed to rule checks
+
   class Doc
-    attr_reader :lines, :parsed, :elements
+    ##
+    # A list of raw markdown source lines. Note that the list is 0-indexed,
+    # while line numbers in the parsed source are 1-indexed, so you need to
+    # subtract 1 from a line number to get the correct line. The element_line*
+    # methods take care of this for you.
+
+    attr_reader :lines
+
+    ##
+    # A Kramdown::Document object containing the parsed markdown document.
+
+    attr_reader :parsed
+
+    ##
+    # A list of top level Kramdown::Element objects from the parsed document.
+
+    attr_reader :elements
+
+    ##
+    # Create a new document given a string containing the markdown source
 
     def initialize(text)
       @lines = text.split("\n")
@@ -12,22 +33,28 @@ module MarkdownLint
       add_levels(@elements)
     end
 
+    ##
+    # Alternate 'constructor' passing in a filename
+
     def self.new_from_file(filename)
-      # Alternate 'constructor' passing in a filename
       self.new(File.read(filename))
     end
 
-    def add_levels(elements, level=1)
-      # Adds a 'level' option to all elements to show how nested they are
-      elements.each do |e|
-        e.options[:element_level] = level
-        add_levels(e.children, level+1)
-      end
-    end
+    ##
+    # Find all elements of a given type, returning their options hash. The
+    # options hash has most of the useful data about an element and often you
+    # can just use this in your rules.
+    #
+    #   # Returns [ { :location => 1, :element_level => 2 }, ... ]
+    #   elements = find_type(:li)
 
     def find_type(type)
       find_type_elements(type).map { |e| e.options }
     end
+
+    ##
+    # Find all elements of a given type, returning a list of the element
+    # objects themselves.
 
     def find_type_elements(type, elements=@elements)
       results = []
@@ -40,23 +67,46 @@ module MarkdownLint
       results
     end
 
+    ##
+    # Returns the line number a given element is located on in the source
+    # file. You can pass in either an element object or an options hash here.
+
     def element_linenumber(element)
       element = element.options if element.is_a?(Kramdown::Element)
       element[:location]
     end
 
+    ##
+    # Returns the actual source line for a given element. You can pass in an
+    # element object or an options hash here. This is useful if you need to
+    # examine the source line directly for your rule to make use of
+    # information that isn't present in the parsed document.
+
     def element_line(element)
       @lines[element_linenumber(element) - 1]
     end
+
+    ##
+    # Returns a list of line numbers for all elements passed in. You can pass
+    # in a list of element objects or a list of options hashes here.
 
     def element_linenumbers(elements)
       elements.map { |e| element_linenumber(e) }
     end
 
+    ##
+    # Returns the actual source lines for a list of elements. You can pass in
+    # a list of elements objects or a list of options hashes here.
+
     def element_lines(elements)
-      # Gets the raw text lines where elements are located
       elements.map { |e| element_line(e) }
     end
+
+    ##
+    # Returns the header 'style' - :atx (hashes at the beginning), :atx_closed
+    # (atx header style, but with hashes at the end of the line also), :setext
+    # (underlined). You can pass in the element object or an options hash
+    # here.
 
     def header_style(header)
       if header.type != :header
@@ -74,6 +124,11 @@ module MarkdownLint
       end
     end
 
+    ##
+    # Returns the bullet style for an unordered list - :asterisk, :plus,
+    # :dash, depending on which symbol is used to denote the list item. You
+    # can pass in either the element itself or an options hash here.
+
     def bullet_style(bullet)
       if bullet.type != :li
         raise "bullet_style called with non-bullet element"
@@ -90,9 +145,25 @@ module MarkdownLint
       end
     end
 
+    ##
+    # Returns how much a given line is indented. Hard tabs are treated as an
+    # indent of 8 spaces. You need to pass in the raw string here.
+
     def indent_for(line)
-      # Returns how much a given line is indented (hard tabs are 8 spaces)
       return line.match(/^\s*/)[0].gsub("\t", " " * 8).length
     end
+
+    private
+
+    ##
+    # Adds a 'level' option to all elements to show how nested they are
+
+    def add_levels(elements, level=1)
+      elements.each do |e|
+        e.options[:element_level] = level
+        add_levels(e.children, level+1)
+      end
+    end
+
   end
 end
