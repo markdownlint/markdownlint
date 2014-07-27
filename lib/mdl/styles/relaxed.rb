191d0733c34879455d0ae78f0d0a3b44abcd024d
Mark Harrison <mark@mivok.net>
2014-07-27 12:37:00 -0400

    Add two more styles
    Fixes #43
    
    Both of these style files are rough drafts right now and are likely to be
    changed.

---
+++ lib/mdl/styles/relaxed.rb
@@ -0,0 +1,8 @@
+all
+exclude_tag :specific_style
+exclude_tag :whitespace
+exclude_tag :line_length
+
+exclude_rule 'MD006' # Lists at beginning of line
+exclude_rule 'MD007' # List indentation
+exclude_rule 'MD008' # List indentation
