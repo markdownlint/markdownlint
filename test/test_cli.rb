markdownlint/markdownlint

commit 49eca6370d3d11f34c4ec4d2b6269665e9ecfa9f

Fix crash bug with rule inclusion on the cli
Also, add tests for rule inclusion/exclusion (more tests to come)

Fixes: #108

lib/mdl/cli.rb
@@ -110,14 +110,18 @@ module MarkdownLint
     def self.toggle_list(parts, to_sym=false)
       if parts.class == String
         parts = parts.split(',')
+        inc = parts.select{|p| not p.start_with?('~')}
+        exc = parts.select{|p| p.start_with?('~')}.map{|p| p[1..-1]}
+        if to_sym
+          inc.map!{|p| p.to_sym}
+          exc.map!{|p| p.to_sym}
+        end
+        {:include => inc, :exclude => exc}
+      else
+        # We already converted the string into a list of include/exclude
+        # pairs, so just return as is
+        parts
       end
-      inc = parts.select{|p| not p.start_with?('~')}
-      exc = parts.select{|p| p.start_with?('~')}.map{|p| p[1..-1]}
-      if to_sym
-        inc.map!{|p| p.to_sym}
-        exc.map!{|p| p.to_sym}
-      end
-      {:include => inc, :exclude => exc}
     end
   end
 end

test/test_cli.rb
@@ -2,11 +2,13 @@ require_relative 'setup_tests'
 require 'open3'
 
 class TestCli < Minitest::Test
-  def run_cli(args, stdin = "")
+  def run_cli(args, stdin = "", mdlrc="default_mdlrc")
     mdl_script = File.expand_path("../../bin/mdl", __FILE__)
+    # Load the mdlrc file from the text/fixtures/ directory
+    mdlrc = File.expand_path("../fixtures/#{mdlrc}", __FILE__)
     result = {}
     result[:stdout], result[:stderr], result[:status] = \
-      Open3.capture3(*(%W{bundle exec #{mdl_script}} + args.split),
+      Open3.capture3(*(%W{bundle exec #{mdl_script} -c #{mdlrc}} + args.split),
                     :stdin_data => stdin)
     result[:status] = result[:status].exitstatus
     result
@@ -58,6 +60,27 @@ class TestCli < Minitest::Test
     assert_equal(0, result[:status])
     assert_match(/^Enabled rules:\nMD001 -.*/, result[:stdout])
     assert_match(/MY001 -.*$/, result[:stdout])
+    assert_equal("", result[:stderr])
+  end
+
+  def test_rule_inclusion_cli
+    result = run_cli("-r MD001 -l")
+    assert_equal(0, result[:status])
+    assert_match(/^Enabled rules:\nMD001 -[^\n]+\n$/, result[:stdout])
+    assert_equal("", result[:stderr])
+  end
+
+  def test_rule_exclusion_cli
+    result = run_cli("-r ~MD001 -l")
+    assert_match(/^Enabled rules:\nMD002 -/, result[:stdout])
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+  end
+
+  def test_rule_inclusion_with_exclusion_cli
+    result = run_cli("-r ~MD001,MD039 -l")
+    assert_equal(0, result[:status])
+    assert_match(/^Enabled rules:\nMD039 -/, result[:stdout])
     assert_equal("", result[:stderr])
   end
 end

test/fixtures/default_mdlrc
@@ -1,0 +1,1 @@
+# Blank .mdlrc

commit 903cac37d4bf9106a49b955e974890bf2cf08183

Test rule/tag specification via config file
Also fix a bug introduced in 49eca63

Fixes #108
Refs #42

lib/mdl/cli.rb
@@ -110,6 +110,8 @@ module MarkdownLint
     def self.toggle_list(parts, to_sym=false)
       if parts.class == String
         parts = parts.split(',')
+      end
+      if parts.class == Array
         inc = parts.select{|p| not p.start_with?('~')}
         exc = parts.select{|p| p.start_with?('~')}.map{|p| p[1..-1]}
         if to_sym

test/fixtures/mdlrc_disable_rules
@@ -1,0 +1,1 @@
+rules "~MD001", "~MD002"

test/fixtures/mdlrc_disable_tags
@@ -1,0 +1,1 @@
+tags "ul", "~indentation"

test/fixtures/mdlrc_enable_rules
@@ -1,0 +1,1 @@
+rules "MD001", "MD002"

test/fixtures/mdlrc_enable_tags
@@ -1,0 +1,1 @@
+tags "headers", "whitespace"

test/test_cli.rb
@@ -109,4 +109,51 @@ class TestCli < Minitest::Test
     assert_rules_enabled(result, ["MD039"], true)
     assert_equal("", result[:stderr])
   end
+
+  def test_tag_inclusion_cli
+    result = run_cli("-t headers -l")
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+    assert_rules_enabled(result, ["MD001", "MD002", "MD003"])
+    assert_rules_disabled(result, ["MD004", "MD005", "MD006"])
+  end
+
+  def test_tag_exclusion_cli
+    result = run_cli("-t ~headers -l")
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+    assert_rules_disabled(result, ["MD001", "MD002", "MD003"])
+    assert_rules_enabled(result, ["MD004", "MD005", "MD006"])
+  end
+
+  def test_rule_inclusion_config
+    result = run_cli("-l", "", "mdlrc_enable_rules")
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+    assert_rules_enabled(result, ["MD001", "MD002"], true)
+  end
+
+  def test_rule_exclusion_config
+    result = run_cli("-l", "", "mdlrc_disable_rules")
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+    assert_rules_disabled(result, ["MD001", "MD002"])
+    assert_rules_enabled(result, ["MD003", "MD004"])
+  end
+
+  def test_tag_inclusion_config
+    result = run_cli("-l", "", "mdlrc_enable_tags")
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+    assert_rules_enabled(result, ["MD001", "MD002", "MD009", "MD010"])
+    assert_rules_disabled(result, ["MD004", "MD005"])
+  end
+
+  def test_tag_exclusion_config
+    result = run_cli("-l", "", "mdlrc_disable_tags")
+    assert_equal(0, result[:status])
+    assert_equal("", result[:stderr])
+    assert_rules_enabled(result, ["MD004", "MD030", "MD032"])
+    assert_rules_disabled(result, ["MD001", "MD005"])
+  end
 end