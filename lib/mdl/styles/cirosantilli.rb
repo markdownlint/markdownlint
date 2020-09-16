121a4ff7c7e49d11b189e0363fa48077149ef654
androidseb <theandroidseb@gmail.com>
2020-09-16 18:55:21 -0400

    fixed line length
    Signed-off-by: androidseb <theandroidseb@gmail.com>

--- lib/mdl/styles/cirosantilli.rb
+++ lib/mdl/styles/cirosantilli.rb
@@ -10,5 +10,6 @@ rule 'MD035', :style => '---'
 # explicitly mentioned in the 'email automatic links' section.
 exclude_rule 'MD033'
 
-# File should end with a single newline character - this isn't forbidden by the style guide
+# File should end with a single newline character
+# this isn't forbidden by the style guide
 exclude_rule 'MD047'
