d0f66e13cbe31c789d027f203eda948039cbf840
androidseb <theandroidseb@gmail.com>
2020-09-16 20:40:41 -0400

    updated comment for consistency
    Signed-off-by: androidseb <theandroidseb@gmail.com>

--- lib/mdl/styles/relaxed.rb
+++ lib/mdl/styles/relaxed.rb
@@ -8,4 +8,4 @@ exclude_rule 'MD033' # Inline HTML
 exclude_rule 'MD034' # Bare URL used
 exclude_rule 'MD040' # Fenced code blocks should have a language specified
 exclude_rule 'MD041' # First line in file should be a top level header
-exclude_rule 'MD047' # File should end with a blank line
+exclude_rule 'MD047' # File should end with a single newline character
