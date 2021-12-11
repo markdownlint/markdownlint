a8779bd84a588f844574c4f0dc4674f582e7fd09
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-12-11 23:24:26 +0100

    Fix formatting of test configuration
    Rule MD013 specification in long_lines_code_style.rb had
    overlong configuration line.
    
    Signed-off-by: Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>

--- test/rule_tests/long_lines_code_style.rb
+++ test/rule_tests/long_lines_code_style.rb
@@ -1,3 +1,4 @@
 all
-rule 'MD013', :code_blocks => false, :ignore_code_blocks => true, :tables => false
+rule 'MD013', :code_blocks => false, :ignore_code_blocks => true,
+              :tables => false
 exclude_rule 'MD041'
