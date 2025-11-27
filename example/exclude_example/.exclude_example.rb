07b41ae3b324863249da8c662fbf83039cb831da
A Ottenheimer <gitpublic@ottenheimer.com>
2025-11-26 18:25:14 -0600

    Better example
    Signed-off-by: A Ottenheimer <gitpublic@ottenheimer.com>

---
+++ example/exclude_example/.exclude_example.rb
@@ -0,0 +1,6 @@
+#Example excluding very long lines
+all
+# To exclude all very long lines use the following
+exclude_rule "MD013"
+#To exclude code blocks triggering on long lines, instead use the following
+#rule 'MD013', :ignore_code_blocks => true
---
+++ example/exclude_example/README.md
@@ -0,0 +1,5 @@
+# Example of a custom .mdlrc and style sheet
+
+This directory has a sample .mdlrc file and the style sheet it calls.
+
+It will exclude a very long line like this one that would ordinarily be flagged by mdl as a violation.
