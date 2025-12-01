0dfc6b4be795470a950ed866a537dd695dba0e57
AJRepo <gitpublic@ottenheimer.com>
2025-11-30 18:18:16 -0600

    Update example/exclude_example/.exclude_example.rb
    Co-authored-by: Phil Dibowitz <phil@ipom.com>
    Signed-off-by: A Ottenheimer <gitpublic@ottenheimer.com>

--- example/exclude_example/.exclude_example.rb
+++ example/exclude_example/.exclude_example.rb
@@ -1,6 +1,11 @@
-#Example excluding very long lines
+# This is a style file, which defines the sets of rules to include/exclude and any configuration
+# for those rules. This must be included from your mdlrc using the `style` paramter.
+
+# This is a sample style to exclude very long lines
+
+# First, include all rules:
 all
-# To exclude all very long lines use the following
+# Then, exclude MD013 (long lines), with:
 exclude_rule "MD013"
-#To exclude code blocks triggering on long lines, instead use the following
+# To, instead exclude code blocks triggering on long lines, use the following
 #rule 'MD013', :ignore_code_blocks => true
