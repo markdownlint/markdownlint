aaeb05207238e4621be308c3df728d8eef9f99cd
A Ottenheimer <gitpublic@ottenheimer.com>
2025-12-01 07:28:32 -0600

    lint cleanup, expand documentation
    Signed-off-by: A Ottenheimer <gitpublic@ottenheimer.com>

--- example/exclude_example/.exclude_example.rb
+++ example/exclude_example/.exclude_example.rb
@@ -1,11 +1,16 @@
-# This is a style file, which defines the sets of rules to include/exclude and any configuration
-# for those rules. This must be included from your mdlrc using the `style` paramter.
+# This is an example style file.
+#
+# This file defines the rules to include/exclude and/or parameters
+# of those rules.
+# This must be included from your .mdlrc using the `style` paramter.
 
 # This is a sample style to exclude very long lines
 
 # First, include all rules:
 all
 # Then, exclude MD013 (long lines), with:
-exclude_rule "MD013"
-# To, instead exclude code blocks triggering on long lines, use the following
-#rule 'MD013', :ignore_code_blocks => true
+exclude_rule 'MD013'
+
+# The above excludes all long lines. If, instead, you want to only
+# exclude code blocks with long lines, use the following:
+# rule 'MD013', :ignore_code_blocks => true
