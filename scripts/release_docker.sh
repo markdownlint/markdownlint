0550d1f731dd9519ad5e2432ec87dccc1d0cbc9a
Phil Dibowitz <phil@ipom.com>
2026-08-09 12:11:20 -0700

    Fix docker release script
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- scripts/release_docker.sh
+++ scripts/release_docker.sh
@@ -4,9 +4,10 @@ VERSION=$(ruby -e "require_relative 'lib/mdl/version.rb'; puts MarkdownLint::VER
 echo "Create docker image for $VERSION (y/n) ?"
 read ans
 if [ "$ans" == 'y' ]; then
+    cd tools/docker
     podman build --no-cache -t markdownlint/markdownlint:latest \
-        -t markdownlint/markdownlint:${VERSION?} .
-    podman push markdownlint/markdownlint:latest
+        -t markdownlint/markdownlint:${VERSION?} . && \
+    podman push markdownlint/markdownlint:latest && \
     podman push markdownlint/markdownlint:${VERSION?}
 else
     echo "Bailing out..."
